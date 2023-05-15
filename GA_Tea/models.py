b20f006c97217cce39ae9600ed79bd4ed306785e
jsots <justinsotolongo@gmail.com>
2023-05-15 15:26:20 -0400

    created models and next will be will serializers

--- GA_Tea/models.py
+++ GA_Tea/models.py
@@ -22,7 +22,8 @@ class Post(models.Model):
     pic = models.ImageField(required = false)
     skills_strong = models.CharField(max_length=500)
     skills_weak = models.CharField(max_length=500)
-    timeline = models.DateField()
+    timeline = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "posts")
 
 
 
@@ -30,12 +31,26 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.CharField(max_length=128)
-    body = models.TextFiled()
-    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = "comment")
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = "comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "comments")
 
 
+class Reply(models.Model):
+    author = models.CharField(max_length=30)
+    body = models.TextField()
+    comment = models.ForeignKey(Comment, on_delete = models.CASCADE, related_name = "replies")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "replies")
 
-class Reply()
+JOB_STATUS=[
+    ('Applied'), 
+    ('Interviewed'), 
+    ('Accepted'), 
+    ('Rejected'),
+    ('Interested'),
+]
+class Career(models.Model):
+    company_name = models.CharField(max_length=500)
+    status = models.CharField(Choices=JOB_STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "careers")
 
-class Notebook(models.Model)
-    goals = 
