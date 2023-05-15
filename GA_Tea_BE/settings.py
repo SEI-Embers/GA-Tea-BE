ac21f9690a861d586ebc6f3240b518bf277726bf
jsots <justinsotolongo@gmail.com>
2023-05-15 14:27:39 -0400

    adds requirement psyco, updates db in settings; next install first app

--- GA_Tea_BE/settings.py
+++ GA_Tea_BE/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'GA_Tea_BE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gatea',
+        'USER': 'nine_admin',
+        'PASSWORD': '9259',
+        'HOST': 'localhost'
     }
 }
 
