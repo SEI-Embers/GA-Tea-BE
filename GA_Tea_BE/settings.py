854866d83a54e691a085a925b52170513f909b71
jsots <justinsotolongo@gmail.com>
2023-05-17 12:09:27 -0400

    Debug = true

--- GA_Tea_BE/settings.py
+++ GA_Tea_BE/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qry!a!dm(7*-kl#xj580e8dby!h!h)c!6bj@y(p_ygbu8d*m^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'ga-tea-be-production.up.railway.app']
 
